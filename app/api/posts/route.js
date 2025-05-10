e827642df87c32523430ebd338f944b06dd3cbe0
Harshita <harshitarera@gmail.com>
2025-05-10 16:23:44 +0530

    100525

--- app/api/posts/route.js
+++ app/api/posts/route.js
@@ -1,20 +1,15 @@
 import connectMongo from "../../../utils/connectMongo";
 import PostModel from "../../models/postModels";
 
-export async function GET(){
+export async function GET() {
+  try {
+    await connectMongo();
 
-    try{
+    const postData = await PostModel.find({});
+    const jsonData = postData.map(post => post.toJSON()); 
 
-        await connectMongo()
-        const postData = await PostModel.find({})
-        return Response.json(postData)
-
-    }
-
-    catch(error){
-
-        return Response.json({message : error.message})
-
-    }
+    return Response.json(jsonData); 
+  } catch (error) {
+    return Response.json({ message: error.message }, { status: 500 });
+  }
 }
-
--- app/models/postModels.js
+++ app/models/postModels.js
@@ -6,6 +6,10 @@ const postSchema = new Schema({
     title : String,
     description : String,
     image:String
+},{ toJSON:{ virtuals : true} })
+
+postSchema.virtual('short_description').get(function(){
+     return this.description.substring(0,50) + '...'
 })
 
 const PostModel = models.Post || model('Post', postSchema)
